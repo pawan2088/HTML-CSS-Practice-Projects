c5b3a06d08c82a14e7ac8e4f34bcc6dbadfca7ca
Pawan Kumar <156088958+pawan2088@users.noreply.github.com>
2025-02-09 13:29:11 +0530

    Update script.js

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,12 +1,20 @@
 const boxes=document.querySelectorAll(".box")
+const reset=document.getElementById("reset-btn")
+const WinnerMessage=document.getElementById("msg")
+const newGame_btn=document.getElementById("newGame-btn")
 valO=true
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
+        new Audio("mouse-click-290204.mp3").play();
         if(valO){
             box.innerHTML="O"
+            box.style.color = "white";
+            box.style.fontWeight="bolder"
             valO=false
         }else{
             box.innerHTML="X"
+            box.style.color = "white";
+            box.style.fontWeight="bolder"
             valO=true 
         }
         box.disabled=true
@@ -19,21 +27,46 @@ const checkWinner=()=>{
         let p2=boxes[pattern[1]].innerText
         let p3=boxes[pattern[2]].innerText
         if(p1!=="" && p2!="" && p3 && p3!=""){
-            if(p1==p2 && p2==p3){
-                winnerMsg(p1)
+            if(p1===p2 && p2===p3){
+                boxes[pattern[0]].style.color="red"
+                boxes[pattern[1]].style.color="red"
+                boxes[pattern[2]].style.color="red"
+                winnerMsg(p1,p2,p3)
             }
         }
     }
 }
 const winnerMsg=(p1)=>{
-    document.getElementById("msg").innerText=`${p1}, is the Winner`
+    WinnerMessage.innerText=`${p1}, Won the Game!`
+    newGame()
     disableBoxes()
+}
+const newGame=()=>{
+    newGame_btn.style.display="block"
+    newGame_btn.addEventListener("click",()=>{
+        resetNow()
+    })
 }
 const disableBoxes=()=>{
     for (const element of boxes) {
         element.disabled=true
     }
 }
+const enableBoxes=()=>{
+    for (const element of boxes) {
+        element.disabled=false
+    }
+}
+function resetNow(){
+    boxes.forEach((box)=>{
+        box.innerText=""
+        WinnerMessage.innerText=""
+        newGame_btn.style.display="none"
+        enableBoxes()
+
+    })
+}
+reset.addEventListener("click",resetNow)
 const winPatterns=[
     [0,1,2],
     [3,4,5],
